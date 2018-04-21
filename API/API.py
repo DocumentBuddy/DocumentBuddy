d33bd3f5c9095c2d6cb38ff9d64a3f4594f94d88
Joshua Töpfer <privat@joshuatoepfer.de>
2018-04-21 10:37:06 +0200

    more REST

--- API/API.py
+++ API/API.py
@@ -3,22 +3,36 @@ from sqlite import Sqlite
 
 app = Flask(__name__)
 
+def get_documents_by_keyword(keyword, isLike):
+    if(isLike):
+        documents = sqlite.selectAllIncludingKeyword(keyword)
+    else:
+        documents = sqlite.selectAllPreciseByKeyword(keyword)
+    if len(documents) == 0:
+        abort(404)
+    jsonObjects = []
+    for document in documents:
+        object = {}
+        object['link'] = document[0]
+        object['text'] = document[1]
+        object['doctype'] = document[2]
+        jsonObjects.append(object)
+    return jsonify(jsonObjects)
+
 
 @app.route('/')
 def hello_world():
     return '<center><h1>It works!</h1> <br> <h2>Sincerely yours, DocumentBuddy</h2></center>'
 
 
-@app.route('/database/api/v1.0/documents/keyword/<string:keyword>', methods=['GET'])
+@app.route('/database/api/v1.0/documents/keyword/exact/<string:keyword>', methods=['GET'])
 def get_documents(keyword):
-    documents = sqlite.selectAllPreciseByKeyword(keyword)
-    if len(documents) == 0:
-        abort(404)
-    json = "["
-    for document in documents:
-        json = json + {'link': document[0], 'text': document[1], 'doctype': document[2]}
-    json = json + "]"
-    return jsonify(json)
+    return get_documents_by_keyword(keyword, False)
+
+
+@app.route('/database/api/v1.0/documents/keyword/like/<string:keyword>', methods=['GET'])
+def get_documents(keyword):
+    return get_documents_by_keyword(keyword, True)
 
 
 if __name__ == '__main__':
--- API/testSqllite.py
+++ API/testSqllite.py
@@ -2,13 +2,14 @@ from sqlite import Sqlite
 import pprint
 
 sqlite = Sqlite('./example.db')
-sqlite.truncateDatabase("main")
-print(sqlite.selectAllPreciseByKeyword('keyword1'))
 sqlite.insertData('link',('keyword1', 'keyword2'),'text','kfw_sth')
 sqlite.insertData('/pathToSomeWhere',('Flo','Badjosh'), 'Hallo Hallo', 'chat')
+sqlite.insertData('/pathToSomeWhereElse',('Flo2','Badjosh2'), 'Hallo2 Hallo2', 'chat2')
+sqlite.updateMain('/pathToSomeWhereElse',('link', 'text'),('./PathToNoReturn', 'It works'))
 
 
 # Data as array
+print(sqlite.selectAllPreciseByKeyword(''))
 print(sqlite.selectAllPreciseByKeyword('keyword1'))
 print(sqlite.selectLinkPreciseByKeyword('keyword1'))
 pprint.pprint(sqlite.selectAllIncludingKeyword('keyword'))
@@ -16,5 +17,7 @@ pprint.pprint(sqlite.selectLinkIncludingKeyword('keyword'))
 pprint.pprint(sqlite.selectLinkIncludingKeyword('JOsh'))
 pprint.pprint(sqlite.selectAll('main'))
 
+#sqlite.truncateDatabase('main')
+#sqlite.truncateDatabase('keywords')
 # handle end of connection
 sqlite.closeConnection()
