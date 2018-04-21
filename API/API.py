8f7294c067f77693f29fa3c3d1d04da79a71fe42
Joshua Töpfer <privat@joshuatoepfer.de>
2018-04-21 11:32:36 +0200

    added get all docs to REST API

--- API/API.py
+++ API/API.py
@@ -24,6 +24,18 @@ def hello_world():
     return '<center><h1>It works!</h1> <br> <h2>Sincerely yours, DocumentBuddy</h2></center>'
 
 
+@app.route('/database/api/v1.0/documents/', methods=['GET'])
+def get_all_documents():
+    documents = sqlite.select_all('main')
+    if len(documents) == 0:
+        abort(404)
+    json_objects = []
+    for document in documents:
+        json_object = {'link': document[0], 'text': document[1], 'doctype': document[2]}
+        json_objects.append(json_object)
+    return jsonify(json_objects)
+
+
 @app.route('/database/api/v1.0/documents/keyword/exact/<string:keyword>', methods=['GET'])
 def get_documents_by_keyword_exact(keyword):
     return get_documents_by_keyword(keyword, False)
