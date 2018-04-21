043e700e8cf69fba4b774c9afe242afccf112643
florianproske <florianproske@outlook.com>
2018-04-21 02:37:12 +0200

    Changed sqlite.py to a class and added testSqllite.py for testreasons. all working fine so far

--- src/sqlite/sqlite.py
+++ src/sqlite/sqlite.py
@@ -1,84 +1,55 @@
 # import
-import pprint
 import sqlite3
 
+class Sqlite:
+    def __init__(self):
+        self.conn = sqlite3.connect('example.db')
+        self.c = self.conn.cursor()
 
-# Connection to Database
-def openconnection(dbfilename):
-    conn = sqlite3.connect(dbfilename)
-    c = conn.cursor()
-    return [conn, c]
+    def __init__(self, databasename):
+        self.conn = sqlite3.connect(databasename)
+        self.c = self.conn.cursor()
 
+    # Close Connection
+    def closeConnection(self):
+        self.conn.commit()
+        self.conn.close()
 
-# Close Connection
-def closeConnection(connection):
-    connection.commit()
-    connection.close()
-
-
-# Insert Data
-def insertData(cursor, link, keywords, text, doctype):
-    cursor.execute("INSERT INTO main VALUES (?,?,?)", (link, text, doctype))
-    for keyword in keywords:
-        cursor.execute("INSERT INTO keywords VALUES (?,?)", (link, keyword))
-
-
-# Select
-def selectAllPreciseByKeyword(cursor, keyword):
-    cursor.execute("SELECT * FROM main WHERE link = (SELECT link FROM keywords WHERE keyword=?)", (keyword,))
-    return cursor.fetchall()
+    # Insert Data
+    def insertData(self, link, keywords, text, doctype):
+        self.c.execute("INSERT INTO main VALUES (?,?,?)", (link, text, doctype))
+        for keyword in keywords:
+            self.c.execute("INSERT INTO keywords VALUES (?,?)", (link, keyword))
 
 
-def selectLinkPreciseByKeyword(cursor, keyword):
-    cursor.execute("SELECT DISTINCT link FROM keywords WHERE keyword=?", (keyword,))
-    return cursor.fetchall()
+    # Select
+    def selectAllPreciseByKeyword(self, keyword):
+        self.c.execute("SELECT * FROM main WHERE link = (SELECT link FROM keywords WHERE keyword=?)", (keyword,))
+        return self.c.fetchall()
 
 
-def selectAllIncludingKeyword(cursor, keyword):
-    cursor.execute("SELECT * FROM main WHERE link = (SELECT link FROM keywords WHERE keyword LIKE ?)",
-                   ('%' + keyword + '%',))
-    return cursor.fetchall()
+    def selectLinkPreciseByKeyword(self, keyword):
+        self.c.execute("SELECT DISTINCT link FROM keywords WHERE keyword=?", (keyword,))
+        return self.c.fetchall()
 
 
-def selectLinkIncludingKeyword(cursor, keyword):
-    cursor.execute("SELECT DISTINCT link FROM keywords WHERE keyword LIKE ?", ('%' + keyword + '%',))
-    return cursor.fetchall()
+    def selectAllIncludingKeyword(self, keyword):
+        self.c.execute("SELECT * FROM main WHERE link = (SELECT link FROM keywords WHERE keyword LIKE ?)",
+                       ('%' + keyword + '%',))
+        return self.c.fetchall()
 
 
-def selectAll(cursor, databasename):
-    cursor.execute("SELECT * FROM {}".format(databasename))
-    return cursor.fetchall()
+    def selectLinkIncludingKeyword(self, keyword):
+        self.c.execute("SELECT DISTINCT link FROM keywords WHERE keyword LIKE ?", ('%' + keyword + '%',))
+        return self.c.fetchall()
 
 
-def truncateDatabase(cursor, databasename):
-    cursor.execute("DELETE FROM {}".format(databasename))
-    return cursor.fetchall()
+    def selectAll(self, databasename):
+        self.c.execute("SELECT * FROM {}".format(databasename))
+        return self.c.fetchall()
 
 
-connection = openconnection('example.db')
-conn = connection[0]
-cursor = connection[1]
-c = cursor
-# Statement Create Table
-# c.execute("DROP TABLE main")
-c.execute("CREATE TABLE IF NOT EXISTS main (link text, text text, doctype text)")
-c.execute("CREATE TABLE IF NOT EXISTS keywords (link text, keyword text)")
-# Truncate
-# truncateDatabase(c, 'main')
-# truncateDatabase(c, 'keywords')
+    def truncateDatabase(self, databasename):
+        self.c.execute("DELETE FROM {}".format(databasename))
+        return self.c.fetchall()
 
-# Statement Insert a precise values
-# insertData(c,'link',('keyword1', 'keyword2'),'text','kfw_sth')
-# insertData(c, '/pathToSomeWhere',('Flo','Badjosh'), 'Hallo Hallo', 'chat')
-
-
-# Data as array
-print(selectAllPreciseByKeyword(cursor, 'keyword1'))
-print(selectLinkPreciseByKeyword(cursor, 'keyword1'))
-pprint.pprint(selectAllIncludingKeyword(cursor, 'keyword'))
-pprint.pprint(selectLinkIncludingKeyword(cursor, 'keyword'))
-pprint.pprint(selectLinkIncludingKeyword(cursor, 'JOsh'))
-pprint.pprint(selectAll(cursor, 'main'))
-
-# handle end of connection
-closeConnection(conn)
